f78ca84d487c2b00d8f5eda63a75fea0c95f983d
Tu Ngo <tungo8810@gmail.com>
2017-04-24 11:26:44 -0700

    Add PostController #new #create #show #edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,36 @@
 class PostsController < ApplicationController
+  before_action :require_author!, only: [:edit, :update]
   def new
+    @post = Post.new
   end
 
   def create
-  end
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
 
-  def index
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :url, :content, :sub_id)
   end
 end
