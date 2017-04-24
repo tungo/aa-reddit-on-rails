e055caa267613e81ac808e482a26a568fbd4d844
Tu Ngo <tungo8810@gmail.com>
2017-04-24 14:43:18 -0700

    Add Comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-
-  get 'comments/create'
-
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 
   resources :subs
-  resources :posts, except: [:index]
+  resources :posts, except: [:index] do
+    resources :comments, only: [:new]
+  end
+
+  resources :comments, only: [:create]
 
   root to: "subs#index"
 end
