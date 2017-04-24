832a99fc05921faf35b97b45ff920fc00dab373d
Tu Ngo <tungo8810@gmail.com>
2017-04-24 10:44:56 -0700

    Add Subs migration

---
+++ app/controllers/subs_controller.rb
@@ -0,0 +1,19 @@
+class SubsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy] 
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :subs
+
 end
