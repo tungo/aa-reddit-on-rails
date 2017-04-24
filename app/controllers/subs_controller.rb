b66cab49469bc3f08d1410b025ec1163bbd0d735
Tu Ngo <tungo8810@gmail.com>
2017-04-24 10:49:01 -0700

    Add SubsController #new #create

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,8 +1,18 @@
 class SubsController < ApplicationController
   def new
+    @sub = Sub.new
   end
 
   def create
+    @sub = Sub.new(sub_params)
+    @sub.moderator_id = current_user.id
+
+    if @sub.save
+      redirect_to sub_link(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
   end
 
   def index
@@ -15,5 +25,9 @@ class SubsController < ApplicationController
   end
 
   def update
+  end
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
   end
 end
--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :integer          not null, primary key
+#  title        :string           not null
+#  description  :string           not null
+#  moderator_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Sub < ActiveRecord::Base
   validates :title, :moderator, presence: true
 
