7022a2ee0b3ca27d4ca2d2b75c31943bf2a503a6
Tu Ngo <tungo8810@gmail.com>
2017-04-24 11:02:03 -0700

    Add subs/show view and fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :auth_token
+  helper_method :current_user, :auth_token, :logged_in?
 
   def user_params
     params.require(:user).permit(:username, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :subs
 
+  root to: "subs#index"
 end
