ad899a99829d3d84631df73d818fe45946b2dec7
Tu Ngo <tungo8810@gmail.com>
2017-04-24 10:23:05 -0700

    Add UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
 
   def new
+
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
-  
 end
