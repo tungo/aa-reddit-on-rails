6d7d2ca580800e1d1c73b7a3427849506072604a
Tu Ngo <tungo8810@gmail.com>
2017-04-24 14:00:54 -0700

    Update PostSub validations

--- app/models/post_sub.rb
+++ app/models/post_sub.rb
@@ -1,5 +1,7 @@
 class PostSub < ActiveRecord::Base
-  validates :post_id, :sub_id, presence: true
+  validates :post, :sub, presence: true
+
+  validates :post_id, uniqueness: { scope: :sub_id }
 
   belongs_to :post,
     foreign_key: :post_id,
